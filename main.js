4d3a3c83963c6faf4feae232b1fb609e7de3f405
igorfs10 <igorfs10@gmail.com>
2020-07-30 20:51:01 -0300

    um pouco da construcao da pagina

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Teste");
+let quantidade_jogadores = 6;
+let dinheiro_jogadores = [ 0, 0, 0, 0, 0, 0 ];
+
+console.log(quantidade_jogadores);
+console.log(dinheiro_jogadores);
+
+const cam = document.getElementById("cam");
+
+const QR_SCANNER = new QrScanner(cam, result => console.log('decoded qr code:', result));
+// QR_SCANNER.start();
